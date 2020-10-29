1f415ecd2362da4f2b6d50d2dd222267c4e46f38
arrontp <arrontp@btinternet.com>
2020-10-29 15:59:56 +0000

    added colourcode project and updated index

---
+++ colourcode/colour_gen.js
@@ -0,0 +1,73 @@
+// To Do:
+// HTML Validation Message on capitals.
+// Hint Button
+// Add icon to Reset
+// Colour Theroy
+// Contrast
+
+var textRequest = new XMLHttpRequest();
+textRequest.open("GET", "htmlcolours2020.txt", true);
+textRequest.send(null);
+textRequest.onreadystatechange = function() {
+  if ( textRequest.readyState === 4 && textRequest.status === 200 ) {
+    const textTo = textRequest.responseText;
+    const loTxt = textTo.toLocaleLowerCase();
+    const colours = loTxt.split(/\r?\n|\r/);
+    const coloursCapz = textTo.split(/\r?\n|\r/);
+    colourCount(colours, coloursCapz); 
+  }
+}
+
+function colourCount(colours, coloursCapz){
+  var fInput = document.getElementById("fInput");
+  fInput.addEventListener("input", letters);
+
+  function letters(letterpressed){ 
+    // console.log(letterpressed.data);
+    var check = colours.includes(fInput.value);
+    if( check == false){
+      // console.log('false');
+    }
+    else{
+      // console.log('true');
+      var colourName = fInput.value;
+      document.getElementById("body").style.backgroundColor=colourName;
+      var clearInput = fInput.value = '';
+      updateDetails(coloursCapz);
+      return clearInput;
+    }
+    var resetBtn = document.getElementById("restart");
+    resetBtn.addEventListener("click", reset);
+
+    function reset(){
+      document.getElementById("body").style.backgroundColor="#f0f0f0";
+      output.style.display="none";
+    }
+
+    function updateDetails(){    
+      document.getElementById("output").style.display="block";
+      var list = document.getElementById("details").getElementsByTagName("li");
+      var cPos = colours.indexOf(colourName);
+      list[0].innerHTML = coloursCapz[cPos];     
+      list[1].innerHTML = colours[cPos + 1]; 
+      // list[2].innerHTML =colour.theory;     
+    }
+  }
+}
+
+
+function hint(){
+  const getHint = document.getElementById("hint");
+  getHint.style.display="block";
+  // const closeBtn = document.getElementsByClassName("closeBtn");
+  // // closeBtn.style.cursor="pointer";
+  // console.log(closeBtn);
+
+
+document.querySelector(".closeBtn").style.cursor="pointer"; 
+
+}
+function hideHint(){
+  const getHint = document.getElementById("hint");
+  getHint.style.display="none";
+}
